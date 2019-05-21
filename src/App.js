cb85c5e9bed3da6b78ef398e25dcd75e1d3a9198
Marquinhus Gonçalves <marcus.goncalves@loggi.com>
2019-05-21 11:09:10 -0300

    Advanced state and Scalable components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 // App.js
-import React, { useState } from 'react';
-import { Normalize, Grid, Typography } from '@smooth-ui/core-sc';
-import SearchInput from './components/SearchInput';
+import React, { useState } from 'react'
+import { Row, Col, Normalize, Grid, Typography } from '@smooth-ui/core-sc'
+import SearchInput from './components/SearchInput'
 import Catch from './components/Catch'
 import { MovieSearch } from './containers/MovieDb'
+import Card from './components/Card'
 
 export default function App() {
   /* TODO: With the help of "useState", you should connect the "SearchInput" field */
@@ -24,11 +25,32 @@ export default function App() {
         />
         <Catch>
           {/* Affichage de la liste de films */}
-          <MovieSearch query={query}>
-            {movies =>
-              movies && movies.map(movie => <p key={movie.id}>{movie.title}</p>)
-            }
-          </MovieSearch>
+          <Row>
+            <MovieSearch query={query}>
+              {movies =>
+                movies && movies.map(movie => (
+                  <Col my={1} xs={12} md={6} key={movie.id}>
+                    <Card
+                      key={movie.id}
+                      style={{
+                        height: 200,
+                        backgroundImage: movie.backdrop_path
+                          ? `url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`
+                          : null,
+                      }}
+                    >
+                      <Card.Body>
+                        <Card.Title>{movie.title}</Card.Title>
+                        <Card.Subtitle>
+                          {movie.vote_average} ({movie.vote_count} votes)
+                        </Card.Subtitle>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                ))
+              }
+            </MovieSearch>
+          </Row>
         </Catch>
       </Grid>
     </>
